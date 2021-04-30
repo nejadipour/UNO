b27539c38f8b9d930d22136bab9163bf54d4669e
nejadipour <nejadipour@gmail.com>
2021-04-30 16:53:06 +0430

    feat: new cards ready to play.

---
+++ src/A.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class A extends Action
+{
+    public A(String name, int point, String color)
+    {
+        super(name, point, color);
+        this.explainAction = "next player can't play in this round.";
+
+    }
+
+
+    @Override
+    public void runAction(RunGame runGame)
+    {
+        runGame.nextRound();
+        runGame.nextRound();
+
+    }
+
+}
---
+++ src/Ten.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Ten extends Action
+{
+    public Ten(String name, int point, String color)
+    {
+        super(name, point, color);
+        this.explainAction = "The direction of the players will change.";
+
+    }
+
+
+    public void changeDirection(RunGame runGame, String direction)
+    {
+        if (direction.equals("clockwise"))
+            runGame.setDirection("anticlockwise");
+        else
+            runGame.setDirection("clockwise");
+
+        Collections.reverse(runGame.getPlayers());
+
+    }
+
+
+    @Override
+    public void runAction(RunGame runGame)
+    {
+        changeDirection(runGame, runGame.getDirection());
+        runGame.nextRound();
+
+    }
+
+}
